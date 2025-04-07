11ff4468b8a06ca4c289dea0d15f9833fabcaaa8
nardos <n73657971@gmail.com>
2025-04-07 16:03:28 -0700

    add the binary search recursive

---
+++ binarysearchrecursive.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int binarySearchRecursive(int arr[], int left, int right, int target) {
+    for (; left <= right;) {
+        int mid = left + (right - left) / 2;
+        
+        if (arr[mid] == target) {
+            return mid;
+        } else if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    
+    return -1;
+}
+
+int main() {
+    int arr[] = {1, 3, 5, 7, 9, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 7;
+    
+    int result = binarySearchRecursive(arr, 0, n - 1, target);
+    if (result != -1) {
+        cout << "Element found at index " << result << endl;
+    } else {
+        cout << "Element not found" << endl;
+    }
+
+    return 0;
+}
